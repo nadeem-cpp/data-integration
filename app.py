70826af96782f052ce5d00af7fa7c0cde470b33f
N-PC <muhammadnad1001100@gmail.com>
2023-06-02 11:02:17 +0500

    file storage in local

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
     return render_template("home.html")
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
     files = request.files.getlist("files")
-    try:
-        for file in files:
-            file.save(dst=f"uploaded/{file.filename}")
-        return "uploaded"
-    except Exception as e:
-        return redirect(url_for("home"), code=302,)
+    print(files[0].filename)
+    # try:
+    #     for file in files:
+    #         file.save(dst=f"uploaded/{file.filename}")
+    #     return "uploaded"
+    # except Exception as e:
+    return redirect(url_for("home"), code=302,)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
