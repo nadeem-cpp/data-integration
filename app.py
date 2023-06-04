cf4d024bd28680cc452fc0edfffd5b1b017e53be
N-PC <muhammadnad1001100@gmail.com>
2023-06-04 17:57:28 +0500

    added bootstrap and bussiness logic for data matching

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from flask import Flask, render_template, redirect, request, url_for
-
+from model import DataHandler
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():  # put application's code here
+def home():
     return render_template("home.html")
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
+    # TODO 1 get files from form and send it to Data Handler object
     files = request.files.getlist("files")
-    print(files[0].filename)
-    # try:
-    #     for file in files:
-    #         file.save(dst=f"uploaded/{file.filename}")
-    #     return "uploaded"
-    # except Exception as e:
-    return redirect(url_for("home"), code=302,)
+    try:
+
+        dh = DataHandler(data_files=files)
+        titles = dh.get_similar_titles()
+        return render_template("matching.html", titles=titles)
+
+    except Exception as e:
+        print(e)
+        return redirect(url_for("home"), code=302,)
+
+
+data = [('fruit', 'country'), ('brand', 'drink'), ('pet', 'Has Car'), ('Likes Pizza', 'Plays Games'),
+        ('fruit', 'country'), ('brand', 'drink'), ('pet', 'Has Car'), ('Likes Pizza', 'Plays Games'),
+        ('fruit', 'country'), ('brand', 'drink'), ('pet', 'Has Car'), ('Likes Pizza', 'Plays Games')]
+
+
+@app.route("/matching")
+def matching():
+    return render_template("matching.html", titles=data, enumerate=enumerate)
 
 
 if __name__ == '__main__':
---
+++ model.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class DataHandler:
+    def __init__(self, data_files: list, separator: str = ","):
+        self.__data_files = data_files
+        self.__sep = separator
+        self.__data_list = []
+        for data_file in data_files:
+            data = pd.read_csv(data_file, sep=self.__sep)
+            self.__data_list.append(data)
+
+    def get_similar_titles(self):
+        """return a list of tuples where each tuple show column names that are going to get merged"""
+        columns = []
+        for data in self.__data_list:
+            columns.append(data.columns.tolist())
+
+        main_remove = []
+        similar = []
+        # TODO 1 create a tuples of columns with same names or similar names like name, sure name
+        for i in range(1, len(columns)):
+            for j, name1 in enumerate(columns[0]):
+                for k, name2 in enumerate(columns[i]):
+                    if name1 == name2:
+                        similar.append((name1, name2))
+                        main_remove.append(name1)
+                        del columns[i][k]
+                    elif name1 in name2:
+                        similar.append((name1, name2))
+                        main_remove.append(name1)
+                        del columns[i][k]
+
+        # remove matched data from main_dataset to avoid extra computation
+        for name in main_remove:
+            columns[0].remove(name)
+
+        columns_temp = self.__cosine_similarity(columns)
+
+        if len(columns_temp) is not 0:
+            similar.append(columns_temp)
+
+        return similar
+
+    def __cosine_similarity(self, unmatched: list):
+        # TODO 2  apply cosine similarity on remaining columns
+        # getting a portion of dataset whose columns names are different
+        dataframes = []
+        for i, unmatch in enumerate(unmatched):
+            dataframes.append(self.__data_list[i].loc[:, unmatch])
+
+        vectorizer = TfidfVectorizer()
+        features = []
+        # transform datasets into scaled valued understandable by model
+        for df in dataframes:
+            features.append(vectorizer.fit_transform(df))
+
+        # cosine similarity
+        # calculate cosine similarity between datasets and append them is a list
+        cosine_similarity_matrix = []
+        for i in range(len(features)):
+            for j in range(i + 1, len(features)):
+                similarity_matrix_temp = cosine_similarity(features[i], features[j])
+                cosine_similarity_matrix.append(similarity_matrix_temp)
+
+        threshold = 0.8
+        similar_columns = []
+
+        # find similarity with respect to threshold on each calculated similarity matrix
+        for similarity_matrix in cosine_similarity_matrix:
+            most_similar_indices = similarity_matrix.argmax(axis=1)
+            for i, j in enumerate(most_similar_indices):
+                similarity_score = similarity_matrix[i, j]
+                if similarity_score > threshold:
+                    similar_columns.append((dataframes[i].columns, dataframes[j].columns))
+
+        return similar_columns
+
+    def __data_cleaning(self):
+        pass
+
+
+# TODO 3 let user decide if we have choosed right columns for merge
+
+
